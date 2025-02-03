59844a079fc0e3adeb31feaaa112d4775cce503e
Duvan Cordoba <duvan.cordoba@blaucastmedia.com>
2025-02-02 19:08:38 -0500

    Add: db with models guitars

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
+import { db } from "./data/db"
+
 function App() {
+
+  const [data, setData] = useState(db)
 
   return (
     <>
@@ -12,7 +16,10 @@ function App() {
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
-            <Guitar />
+            {data.map((guitar) => (
+                <Guitar key={guitar.id} guitar={guitar} />
+            ))
+            }
         </div>
     </main>
 
